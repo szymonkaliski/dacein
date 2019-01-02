7a24221754c1670c2a1e5137d9804c878513b5ac
Szymon Kaliski <hi@szymonkaliski.com>
2019-01-02 16:38:37 +0100

    basic editor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState, useRef } from "react";
 import lodash, { get } from "lodash";
 import Immutable, { Map, List } from "immutable";
+import AceEditor from "react-ace";
+
+import "brace/mode/javascript";
+import "brace/theme/github";
 
 // expose to sketch once it's eval()-ed
 window.Immutable = Immutable;
@@ -8,9 +12,13 @@ window.lodash = lodash;
 
 const MAX_HISTORY_LEN = 1000;
 
-const sketch = {
+const TEST_SKETCH = `
+const { Map } = Immutable;
+const { range } = lodash;
+
+sketch({
   setup: {
-    canvas: [800, 400]
+    canvas: [600, 600]
   },
 
   initialState: Map({ c: 0 }),
@@ -20,62 +28,18 @@ const sketch = {
   },
 
   draw: state => {
-    const points = lodash
-      .range(300)
-      .map(i => [
-        Math.sin((state.get("c") + i * 0.2) * 0.1) * 330 + 400,
-        Math.cos((state.get("c") + i * 0.2) * 0.5) * 100 + 200
-      ]);
+    const points = range(300).map(i => [
+      Math.sin((state.get("c") + i * 0.2) * 0.1) * 200 + 300,
+      Math.cos((state.get("c") + i * 0.2) * 0.3) * 200 + 300
+    ]);
 
     return [
       ["background", { fill: "#eee" }],
       ["path", { points, stroke: "#333" }]
-
-      // ...lodash.range(40).map(i => {
-      //   return [
-      //     "rect",
-      //     {
-      //       pos: [
-      //         Math.sin((state.get("c") + i) * 0.1) * 330 + 400,
-      //         Math.cos((state.get("c") + i) * 0.5) * 100 + 200
-      //       ],
-      //       size: [40, 40],
-      //       fill: "#ddd",
-      //       stroke: "#333"
-      //     }
-      //   ];
-      // }),
-
-      // ...lodash.range(40).map(i => {
-      //   return [
-      //     "line",
-      //     {
-      //       a: [
-      //         Math.sin((state.get("c") + i) * 0.1) * 330 + 400,
-      //         Math.cos((state.get("c") + i) * 0.5) * 100 + 200
-      //       ],
-      //       b: [400, 200],
-      //       stroke: "#333"
-      //     }
-      //   ];
-      // }),
-
-      // ...lodash.range(40).map(i => {
-      //   return [
-      //     "ellipse",
-      //     {
-      //       pos: [
-      //         Math.sin((state.get("c") + i) * 0.1) * 330 + 400,
-      //         Math.cos((state.get("c") + i) * 0.5) * 100 + 200
-      //       ],
-      //       size: [4, 4],
-      //       fill: "#333"
-      //     }
-      //   ];
-      // })
     ];
   }
-};
+})
+`;
 
 const COMMANDS = {
   background: (ctx, { fill }, { width, height }) => {
@@ -265,7 +229,7 @@ const Sketch = ({ sketch }) => {
           max={history.size - 1}
           step={1}
           value={historyIdx}
-          onChange={e => updateHistory([history, e.target.value])}
+          onChange={e => updateHistory([history, parseInt(e.target.value, 10)])}
         />
 
         <button
@@ -290,8 +254,46 @@ const Sketch = ({ sketch }) => {
   );
 };
 
-export default () => (
-  <div className="sans-serif pa2">
-    <Sketch sketch={sketch} />
-  </div>
-);
+const Editor = ({ sketch, onChange }) => {
+  return (
+    <AceEditor
+      mode="javascript"
+      theme="github"
+      value={sketch}
+      width="800px"
+      showGutter={false}
+      showPrintMargin={false}
+      onChange={e => onChange(e)}
+    />
+  );
+};
+
+export default () => {
+  const [code, setCode] = useState(TEST_SKETCH);
+  const [sketch, setSketch] = useState(null);
+
+  useEffect(
+    () => {
+      if (!window.sketch) {
+        window.sketch = sketch => setSketch(sketch);
+      }
+
+      try {
+        eval(`const sketch = window.sketch; ${code}`);
+      } catch (e) {}
+
+      return () => delete window.sketch;
+    },
+    [code]
+  );
+
+  return (
+    <div className="sans-serif pa2 flex">
+      {sketch && <Sketch sketch={sketch} />}
+
+      <div className="ml2 ba b--light-gray">
+        <Editor sketch={code} onChange={e => setCode(e)} />
+      </div>
+    </div>
+  );
+};
