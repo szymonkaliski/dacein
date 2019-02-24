abc5a70ac98705a871c96d5ec7a854619bd97974
Szymon Kaliski <hi@szymonkaliski.com>
2019-02-24 10:22:34 +0100

    Updates

---
+++ precache-manifest.cbdf15a6fc93eaa60e9c54e3f896935e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b63705c1f5068307351",
+    "url": "/dacein/static/css/main.fa95258d.chunk.css"
+  },
+  {
+    "revision": "1b63705c1f5068307351",
+    "url": "/dacein/static/js/main.1b63705c.chunk.js"
+  },
+  {
+    "revision": "cdfd37fc86ebf7dc1f43",
+    "url": "/dacein/static/js/runtime~main.cdfd37fc.js"
+  },
+  {
+    "revision": "c78dbde73e6ecf885c05",
+    "url": "/dacein/static/css/2.95f9a1a9.chunk.css"
+  },
+  {
+    "revision": "c78dbde73e6ecf885c05",
+    "url": "/dacein/static/js/2.c78dbde7.chunk.js"
+  },
+  {
+    "revision": "52272ed8cf61d4a41f0521a316033dea",
+    "url": "/dacein/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d7300923e3cc38381bbed821f779c970.js"
+  "/dacein/precache-manifest.cbdf15a6fc93eaa60e9c54e3f896935e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/dacein/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
